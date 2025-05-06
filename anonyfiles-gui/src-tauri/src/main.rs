ced58631999b37817f64fd5f269da835b86ee867
Simon Grossi <simon.grossi@gmail.com>
2025-05-06 13:15:05 +0200

    Update CLI (bug)

--- anonyfiles-gui/src-tauri/src/main.rs
+++ anonyfiles-gui/src-tauri/src/main.rs
@@ -1,16 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
-#[tauri::command]
-fn process_file(path: String) {
-    println!("Processing file at path: {}", path);
-    // Tu pourras ici appeler Python si nécessaire
-}
-
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![process_file])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  app_lib::run();
 }
